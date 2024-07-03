51640f5029d8261bfbbb19e6fac95ded92186382
Vinayak Shetty <shetty394@gmail.com>
2024-07-03 16:41:32 +0530

    Vinayak has made changes to the structure

--- src/test/java/Snapwork/Tests/SelfAccountTransferTest.java
+++ src/test/java/Snapwork/Tests/SelfAccountTransferTest.java
@@ -2,7 +2,7 @@ package Snapwork.Tests;
 
 import org.testng.annotations.Test;
 
-import Snapwork.PageObjects.Dashboard;
+import Snapwork.PageObjects.Coachguide;
 import Snapwork.PageObjects.SelfAccountTransfer;
 import Snapwork.TestComponents.BaseTest;
 
@@ -10,7 +10,7 @@ public class SelfAccountTransferTest extends BaseTest{
 
 	@Test
 	public void selfTransferVerify() throws InterruptedException {
-		Dashboard dashboard = loginScreenP.loginApplication("510284", "Idbi@123");
+		Coachguide dashboard = loginScreenP.loginApplication("510284", "Idbi@123");
 		SelfAccountTransfer selfAccountTransfer = dashboard.completeCoachGuide();
 		selfAccountTransfer.clickFromAccountDropdown();
 		System.out.println("Account Dropdown Clicked");
